f1d72814b07a3a0e6bb0ed4911050e137d5c693d
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-05-12 13:43:55 +0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {ContextMenu} from "./components/ContextMenu";
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <ContextMenu/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ContextMenu.jsx
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+function ContextMenu(){
+    return (
+         <h1>menu</h1>
+    )
+}
+
+export {ContextMenu}
